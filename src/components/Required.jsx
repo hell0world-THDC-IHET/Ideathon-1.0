6ea304ba4e6a59833287b6ee26cfdfa5cd9489db
Akatsuki-Uchiha-Itachi <raviarya4542@gmail.com>
2024-03-22 22:59:14 +0530

    updated: 22 march 2024

---
+++ src/components/Required.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import bannerguy from '../assets/img/bannerguy.svg'
+
+const Required = () => {
+  return (
+    <div>
+      <img src={bannerguy} alt="" />
+    </div>
+  )
+}
+
+export default Required
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Progess from '../components/Progess'
+import Designer from '../components/Required'
 
 const Home = () => {
   return (
     <div className="home">
         <Header/>
         <Navbar/>
-        <Banner/>
-        <Banner/>
-        <Banner/>
-        <Banner/>
+        <Progess/>
+        <Designer/>
     </div>
   )
 }
